ac720a0a2929ff53a0797b2feae3d76aa865dcd0
Patrick McCall <patrick.mccall@gmail.com>
2016-01-28 19:40:48 -0800

    updates

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -6,3 +6,13 @@ imageList[2] = "images/drawing/thumbs/artwork_3.jpg";
 imageList[3] = "images/drawing/thumbs/artwork_4.jpg";
 imageList[4] = "images/drawing/thumbs/artwork_5.jpg";
 imageList[5] = "images/drawing/thumbs/artwork_6.jpg";
+
+
+//hook into the thumbs div
+var thumbsDiv = document.getElementById("thumbs");
+
+thumbsDiv.innerHTML = '<img src="' + imageList[1] + '" />';
+
+window.onload = function () {
+  buildThumbnails();//use or "fire off" this function
+};
