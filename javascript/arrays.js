1a8fdd476ab64dccf43389e79389716ba840cc8f
Patrick McCall <patrick.mccall@gmail.com>
2016-01-28 18:49:34 -0800

    first

---
+++ javascript/arrays.js
@@ -0,0 +1,8 @@
+﻿var colorList = ["red", "black", "white", "blue", "ocher", "yellow", "pink"];
+var imageList = new Array();
+imageList[0] = "images/drawing/thumbs/artwork_1.jpg";
+imageList[1] = "images/drawing/thumbs/artwork_2.jpg";
+imageList[2] = "images/drawing/thumbs/artwork_3.jpg";
+imageList[3] = "images/drawing/thumbs/artwork_4.jpg";
+imageList[4] = "images/drawing/thumbs/artwork_5.jpg";
+imageList[5] = "images/drawing/thumbs/artwork_6.jpg";
