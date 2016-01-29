61ecca77fe87bd693a8013fc1a1765dd04c7fcb4
Patrick McCall <patrick.mccall@gmail.com>
2016-01-28 19:49:51 -0800

    updated js

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -1,18 +1,30 @@
 ﻿var colorList = ["red", "black", "white", "blue", "ocher", "yellow", "pink"];
-var imageList = new Array();
-imageList[0] = "images/drawing/thumbs/artwork_1.jpg";
-imageList[1] = "images/drawing/thumbs/artwork_2.jpg";
-imageList[2] = "images/drawing/thumbs/artwork_3.jpg";
-imageList[3] = "images/drawing/thumbs/artwork_4.jpg";
-imageList[4] = "images/drawing/thumbs/artwork_5.jpg";
-imageList[5] = "images/drawing/thumbs/artwork_6.jpg";
+
+  var imageList = new Array();
+  imageList[0] = "images/drawing/thumbs/artwork_1.jpg";
+  imageList[1] = "images/drawing/thumbs/artwork_2.jpg";
+  imageList[2] = "images/drawing/thumbs/artwork_3.jpg";
+  imageList[3] = "images/drawing/thumbs/artwork_4.jpg";
+  imageList[4] = "images/drawing/thumbs/artwork_5.jpg";
+  imageList[5] = "images/drawing/thumbs/artwork_6.jpg";
 
 
-//hook into the thumbs div
-var thumbsDiv = document.getElementById("thumbs");
+//create the build thumbnails function
+function buildThumbnails() {
+    //hook into the thumbs div
+  var thumbsDiv = document.getElementById("thumbs");
 
-thumbsDiv.innerHTML = '<img src="' + imageList[1] + '" />';
+  thumbsDiv.innerHTML = '<img src="' + imageList[0] + '" />'
+                      + '<img src="' + imageList[1] + '" />'
+                      + '<img src="' + imageList[2] + '" />'
+                      + '<img src="' + imageList[3] + '" />';
+}
+
+
+
+
 
 window.onload = function () {
-  buildThumbnails();//use or "fire off" this function
+  buildThumbnails();    //use or "fire off" or invoke this function
 };
+
